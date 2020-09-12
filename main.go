bb3e6288ad399a783ae412de3bdbeb3abc724293
Leo Takaoka <tlkanb0908@gmail.com>
2020-09-12 17:38:07 +0900

    apiにした

--- main.go
+++ main.go
@@ -4,23 +4,34 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-	hatebuTrend()
+	http.HandleFunc("/trend", trendHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func qiitaTrend() {
+func trendHandler(w http.ResponseWriter, r *http.Request) {
+	trend, err := qiitaTrend()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintln(w, trend)
+}
+
+func qiitaTrend() (string, error) {
 	doc, err := goquery.NewDocument("https://qiita.com/")
 	if err != nil {
 		log.Fatal(err)
+		return "", err
 	}
 	trend := doc.Find("div[data-hyperapp-app='Trend']")
 	val, exists := trend.Attr("data-hyperapp-props")
 	if !exists {
-		errors.New("data-hyperapp-props does not exist")
+		return "", errors.New("data-hyperapp-props does not exist")
 	}
-	fmt.Println(val)
+	return val, nil
 }
