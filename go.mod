0acdad21b6de2a49ab06ff2d7c89f800662d0d01
Leo Takaoka <tlkanb0908@gmail.com>
2020-09-12 16:16:34 +0900

    qiitaのトレンドを取得

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Le0tk0k/qiita-trend
+
+go 1.15
+
+require github.com/PuerkitoBio/goquery v1.5.1
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	doc, err := goquery.NewDocument("https://qiita.com/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	trend := doc.Find("div[data-hyperapp-app='Trend']")
+	val, exists := trend.Attr("data-hyperapp-props")
+	if !exists {
+		errors.New("data-hyperapp-props does not exist")
+	}
+	fmt.Println(val)
+	fmt.Printf("%T\n", val)
+}
